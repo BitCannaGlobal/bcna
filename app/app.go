8cf2ea4774516915c1f26c93ae1865d649129463
Raul Bernal <bernal.raul@gmail.com>
2022-10-31 11:24:25 +0100

    Update app.go

--- app/app.go
+++ app/app.go
@@ -758,7 +758,7 @@ func (app *App) RegisterTendermintService(clientCtx client.Context) {
 }
 
 func (app *App) RegisterUpgradeHandlers() {
-	planName := "trichomemonster-ica"
+	planName := "trichomemonster-ica-test"
 	app.UpgradeKeeper.SetUpgradeHandler(planName, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		// Set Initial Consensus Version
 		fromVM[icatypes.ModuleName] = app.mm.Modules[icatypes.ModuleName].ConsensusVersion()
