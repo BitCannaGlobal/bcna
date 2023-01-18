1dd4b619a034a486eb04083733559cea8979dfc1
Raul Bernal <bernal.raul@gmail.com>
2023-01-18 13:40:01 +0100

    Adding address verifier

--- cmd/bcnad/cmd/config.go
+++ cmd/bcnad/cmd/config.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/BitCannaGlobal/bcna/app"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
 )
 
 func initSDKConfig() {
@@ -17,5 +20,12 @@ func initSDKConfig() {
 	config.SetBech32PrefixForAccount(app.AccountAddressPrefix, accountPubKeyPrefix)
 	config.SetBech32PrefixForValidator(validatorAddressPrefix, validatorPubKeyPrefix)
 	config.SetBech32PrefixForConsensusNode(consNodeAddressPrefix, consNodePubKeyPrefix)
+	config.SetAddressVerifier(func(bytes []byte) error {
+		n := len(bytes)
+		if (n != 0) && (n <= address.MaxAddrLen) {
+			return nil
+		}
+		return fmt.Errorf("unexpected address length %d", n)
+	})
 	config.Seal()
 }
