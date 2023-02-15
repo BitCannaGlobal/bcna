2e90dc3541eb0f0ebedaa5154e87d8faf3a6c771
Raul Bernal <bernal.raul@gmail.com>
2023-02-15 10:27:18 +0100

    Fix queries with height param at bcna module
    fix `show-bitcannaid [id]` query

--- x/bcna/client/cli/query_bitcannaid.go
+++ x/bcna/client/cli/query_bitcannaid.go
@@ -53,7 +53,10 @@ func CmdShowBitcannaid() *cobra.Command {
 		Short: "shows a bitcannaid",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx := client.GetClientContextFromCmd(cmd)
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
 
 			queryClient := types.NewQueryClient(clientCtx)
 
