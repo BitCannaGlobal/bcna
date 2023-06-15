bf8b5caa5bf1e795a67e8670b492a25ab6eba8c6
Raul Bernal <bernal.raul@gmail.com>
2023-06-15 12:23:06 +0200

    Lint: import not needed

--- cmd/bcnad/cmd/root.go
+++ cmd/bcnad/cmd/root.go
@@ -29,7 +29,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/crisis"
-	"github.com/cosmos/cosmos-sdk/x/genutil"
 	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	"github.com/spf13/cast"
