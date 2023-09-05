7b1c2b18e774048c8a1566615119f7331669cfb2
Raul Bernal <bernal.raul@gmail.com>
2023-09-05 16:31:26 +0200

    improve upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -34,6 +34,8 @@ import (
 	ibctmmigrations "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint/migrations"
 )
 
+var keyTableAssigned bool
+
 // RegisterUpgradeHandlers registers upgrade handlers.
 
 func (app App) RegisterUpgradeHandlers() {
@@ -53,49 +55,70 @@ func (app *App) GanjaRevolution47(_ upgradetypes.Plan) {
 		subspace := subspace
 
 		var keyTable paramstypes.KeyTable
+
 		switch subspace.Name() {
 		case authtypes.ModuleName:
 			keyTable = authtypes.ParamKeyTable() //nolint:staticcheck
+			keyTableAssigned = true
 		case banktypes.ModuleName:
 			keyTable = banktypes.ParamKeyTable() //nolint:staticcheck
+			keyTableAssigned = true
 		case stakingtypes.ModuleName:
 			keyTable = stakingtypes.ParamKeyTable() //nolint:staticcheck
+			keyTableAssigned = true
 		case minttypes.ModuleName:
 			keyTable = minttypes.ParamKeyTable() //nolint:staticcheck
+			keyTableAssigned = true
 		case distrtypes.ModuleName:
 			keyTable = distrtypes.ParamKeyTable() //nolint:staticcheck
+			keyTableAssigned = true
 		case slashingtypes.ModuleName:
 			keyTable = slashingtypes.ParamKeyTable() //nolint:staticcheck
+			keyTableAssigned = true
 		case govtypes.ModuleName:
 			keyTable = govv1.ParamKeyTable() //nolint:staticcheck
+			keyTableAssigned = true
 		case crisistypes.ModuleName:
 			keyTable = crisistypes.ParamKeyTable() //nolint:staticcheck
+			keyTableAssigned = true
 
 		// ibc types
 		case ibctransfertypes.ModuleName:
 			keyTable = ibctransfertypes.ParamKeyTable()
+			keyTableAssigned = true
 		case icahosttypes.SubModuleName:
 			keyTable = icahosttypes.ParamKeyTable()
+			keyTableAssigned = true
 		case icacontrollertypes.SubModuleName:
 			keyTable = icacontrollertypes.ParamKeyTable()
+			keyTableAssigned = true
 
 		// Bitcanna types
 		case bcnamoduletypes.ModuleName:
 			keyTable = bcnamoduletypes.ParamKeyTable() //nolint:staticcheck
+			keyTableAssigned = true
+
+		// Debug:
+		default:
+			fmt.Println("No matching subspace found:", subspace.Name())
+			keyTableAssigned = false
 		}
 
 		if !subspace.HasKeyTable() {
-			subspace.WithKeyTable(keyTable)
+			if !keyTableAssigned {
+				fmt.Println("KeyTable is not assigned for subspace:", subspace.Name())
+			} else {
+				subspace.WithKeyTable(keyTable)
+			}
 		}
 	}
-
-	baseAppLegacySS := app.ParamsKeeper.Subspace(baseapp.Paramspace).WithKeyTable(paramstypes.ConsensusParamsKeyTable())
 
 	app.UpgradeKeeper.SetUpgradeHandler(planName, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info("start to run module migrations...")
 		logger := ctx.Logger().With("upgrade", planName)
 
 		// Migrate Tendermint consensus parameters from x/params module to a dedicated x/consensus module.
+		baseAppLegacySS := app.ParamsKeeper.Subspace(baseapp.Paramspace).WithKeyTable(paramstypes.ConsensusParamsKeyTable())
 		baseapp.MigrateParams(ctx, baseAppLegacySS, &app.ConsensusParamsKeeper)
 
 		// Run migrations
@@ -120,10 +143,23 @@ func (app *App) GanjaRevolution47(_ upgradetypes.Plan) {
 			return nil, err
 		}
 
-		legacyBaseAppSubspace := app.ParamsKeeper.Subspace(baseapp.Paramspace).WithKeyTable(paramstypes.ConsensusParamsKeyTable())
-		baseapp.MigrateParams(ctx, legacyBaseAppSubspace, &app.ConsensusParamsKeeper)
+		// update gov params to use a 20% initial deposit ratio, allowing us to remote the ante handler
+		govParams := app.GovKeeper.GetParams(ctx)
+		govParams.MinInitialDepositRatio = sdk.NewDec(20).Quo(sdk.NewDec(100)).String()
+		if err := app.GovKeeper.SetParams(ctx, govParams); err != nil {
+			return nil, err
+		}
 
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		// x/Staking - set minimum commission to 4,20% 0.042000000000000000
+		stakingParams := app.StakingKeeper.GetParams(ctx)
+		stakingParams.MinCommissionRate = sdk.NewDecWithPrec(420, 4)
+		err = app.StakingKeeper.SetParams(ctx, stakingParams)
+		if err != nil {
+			return nil, err
+		}
+
+		return versionMap, err
+		// return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	})
 
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
