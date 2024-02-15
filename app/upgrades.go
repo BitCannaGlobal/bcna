a36ab86b64d1bcae91c89f3a83b40945feaebbc7
Raul Bernal <bernal.raul@gmail.com>
2024-02-15 10:25:14 +0100

    testing upgrades

--- app/upgrades.go
+++ app/upgrades.go
@@ -8,6 +8,7 @@ import (
 
 	storetypes "cosmossdk.io/store/types"
 	circuittypes "cosmossdk.io/x/circuit/types"
+	ibcfeetypes "github.com/cosmos/ibc-go/v8/modules/apps/29-fee/types"
 
 	// nft "cosmossdk.io/x/nft"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
@@ -34,8 +35,13 @@ func (app *App) StickyFingers(_ upgradetypes.Plan) {
 				app.Logger().Info(fmt.Sprintf("Module: %s, Version: %d", moduleName, version))
 
 			}
-
-			return app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
+			versionMap, err := app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
+			if err != nil {
+				return nil, err
+			}
+			app.Logger().Info(fmt.Sprintf("post migrate version map: %v", versionMap))
+			// return app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
+			return versionMap, err
 		},
 	)
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
@@ -47,7 +53,11 @@ func (app *App) StickyFingers(_ upgradetypes.Plan) {
 		storeUpgrades := storetypes.StoreUpgrades{
 			Added: []string{
 				circuittypes.ModuleName,
-				"feeibc",
+				ibcfeetypes.ModuleName,
+				// ibctm.ModuleName,
+				// solomachine.ModuleName,
+				// runtime.ModuleName,
+				// "ibc-interchainaccounts",
 				// nft.ModuleName,
 			},
 		}
