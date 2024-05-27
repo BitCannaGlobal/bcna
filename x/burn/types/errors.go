73fde19f3ea1b9593d1f159c68dbfaaa0cf595f0
Raul Bernal <bernal.raul@gmail.com>
2024-05-27 14:02:54 +0200

    LINT Error: SA1019: sdkerrors.Register is deprecated

--- x/burn/types/errors.go
+++ x/burn/types/errors.go
@@ -3,11 +3,11 @@ package types
 // DONTCOVER
 
 import (
-	sdkerrors "cosmossdk.io/errors"
+	sdkioerrors "cosmossdk.io/errors"
 )
 
 // x/burn module sentinel errors
 var (
-	ErrInvalidSigner = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
-	ErrSample        = sdkerrors.Register(ModuleName, 1101, "sample error")
+	ErrInvalidSigner = sdkioerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrSample        = sdkioerrors.Register(ModuleName, 1101, "sample error")
 )
