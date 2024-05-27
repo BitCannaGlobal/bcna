a31d9eee6122787274c7198dc009c4f11c5e40b1
Raul Bernal <bernal.raul@gmail.com>
2024-05-27 15:45:56 +0200

    Nolint, rollback last commit

--- x/burn/types/errors.go
+++ x/burn/types/errors.go
@@ -3,11 +3,11 @@ package types
 // DONTCOVER
 
 import (
-	sdkioerrors "cosmossdk.io/errors"
+	sdkerrors "cosmossdk.io/errors"
 )
 
 // x/burn module sentinel errors
 var (
-	ErrInvalidSigner = sdkioerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
-	ErrSample        = sdkioerrors.Register(ModuleName, 1101, "sample error")
+	ErrInvalidSigner = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrSample        = sdkerrors.Register(ModuleName, 1101, "sample error")
 )
